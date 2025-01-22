d13e4705b07a22aae75e5ee92012b30ec09baa1e
Syed Ajmal Bukahri <ajmalbukhari06@gmail.com>
2025-01-22 10:34:21 +0500

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const [items, setItems] = useState([]);
+
+    useEffect(() => {
+        axios.get('/api/items')
+            .then(response => setItems(response.data.items))
+            .catch(err => console.error(err));
+    }, []);
+
+    return (
+        <div>
+            <h1>Buy and Sell Pre-Owned Goods</h1>
+            <ul>
+                {items.map((item, index) => (
+                    <li key={index}>{item}</li>
+                ))}
+            </ul>
+        </div>
+    );
+};
 
 export default App;
